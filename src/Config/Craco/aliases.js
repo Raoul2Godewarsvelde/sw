f4b608d4664d18921479d735beb68cb0da322c0a
MokoMaracucho <desmoulin.federico@gmail.com>
2022-06-04 12:59:40 +0200

    OK: Cracoo aliases

---
+++ src/Config/Craco/aliases.js
@@ -0,0 +1,16 @@
+const aliases = (prefix = `src`) => ({
+	'@public': `${prefix}/public`,
+	'@root': `${prefix}/src`,
+
+	'@assets': `${prefix}/src/Assets`,
+	'@components': `${prefix}/src/Components`,
+	'@config': `${prefix}/src/Config`,
+	'@hooks': `${prefix}/src/Hooks`,
+	'@json': `${prefix}/src/Json`,
+	'@lib': `${prefix}/src/Lib`,
+	'@pages': `${prefix}/src/Pages`,
+	'@styles': `${prefix}/src/Styles`,
+	'@utils': `${prefix}/src/Utils`
+});
+
+module.exports = aliases;

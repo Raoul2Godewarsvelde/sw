055a8b843a6c892b05ca497b439173ac2290b1dc
MokoMaracucho <desmoulin.federico@gmail.com>
2022-06-06 13:03:07 +0200

    OK: SimpleLineChart BUT not real-time...

--- src/Components/Charts/Components/SimpleLineChart.js
+++ src/Components/Charts/Components/SimpleLineChart.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
-const SimpleLineChart = () => {
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+
+const SimpleLineChart = ({ data }) => {
 
     return (
-        <div>SimpleLineChart</div>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
     )
 }
 

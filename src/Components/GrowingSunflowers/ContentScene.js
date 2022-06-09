fb934c69a424364ae5f27cd1a207c96b630cd768
MokoMaracucho <desmoulin.federico@gmail.com>
2022-06-09 22:09:09 +0200

    OK: issue after upgraded R3f...

--- src/Components/GrowingSunflowers/ContentScene.js
+++ src/Components/GrowingSunflowers/ContentScene.js
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import * as THREE from 'three'
 import { useFrame } from '@react-three/fiber'
+/* import { Sampler } from '@react-three/drei' */
 
 import LotusNenupharDRACO from '@assets/Blender/LotusNenupharDRACO.js'
 import PlaneTestDRACO from '@assets/Blender/PlaneTestDRACO.js'
 
 const ContentScene = () => {
 
-    const pointer = new THREE.Vector2();
+    const pointer = new THREE.Vector2()
     const raycaster = new THREE.Raycaster()
 
+    const geomRef = useRef()
+    const flowerRef = useRef()
+
+    const AddLotusNenuphar = () => {
+
+    }
 
     useFrame(({ scene, camera }) => {
         window.addEventListener('pointermove', (e) => {
@@ -18,17 +25,33 @@ const ContentScene = () => {
             pointer.y =  - (e.clientY / window.innerHeight) * 2 + 1
 
             raycaster.setFromCamera(pointer, camera)
-            /* console.log('raycaster', raycaster) */
             
-            /* console.log('scene', scene) */
             const intersects = raycaster.intersectObjects(scene.children[4].children)
-            console.log('intersects', intersects)
+
+            if(intersects.length > 0 ){
+                AddLotusNenuphar()
+            }
         })
     })
 
     return (
         <>
-            <LotusNenupharDRACO />
+            {/* <Sampler
+                transform={({ position, normal, dummy: object }) => {
+                    object.scale.setScalar(Math.random() * 0.0075)
+                    object.position.copy(position)
+                    object.lookAt(normal.add(position))
+                    object.rotation.y += Math.random() - 0.5 * (Math.PI * 0.5)
+                    object.rotation.x += Math.random() - 0.5 * (Math.PI * 0.5)
+                    object.rotation.z += Math.random() - 0.5 * (Math.PI * 0.5)
+                    object.updateMatrix()
+                    return object
+                }}
+                mesh={geomRef}
+                instances={flowerRef}
+                weight="density"
+            /> */}
+            <LotusNenupharDRACO ref={flowerRef} />
             <PlaneTestDRACO />
         </>
     )

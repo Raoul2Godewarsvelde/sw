3a02ef817cc90c74286c2c1174b06dd1b2ac89b5
MokoMaracucho <desmoulin.federico@gmail.com>
2022-06-09 20:40:32 +0200

    OK: window.addEventListener('pointermove', (e) => ...) -> pointer{x, y}

--- src/Components/GrowingSunflowers/ContentScene.js
+++ src/Components/GrowingSunflowers/ContentScene.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import { useFrame } from '@react-three/fiber'
+
 import LotusNenupharDRACO from '@assets/Blender/LotusNenupharDRACO.js'
 import PlaneTestDRACO from '@assets/Blender/PlaneTestDRACO.js'
 
 const ContentScene = () => {
+
+    useFrame((state, delta) => {
+        window.addEventListener('pointermove', (e) => {
+            const pointer = {
+                x: (e.clientX / window.innerWidth) * 2 - 1,
+                y: - (e.clientY / window.innerHeight) * 2 + 1 
+            }
+            console.log('pointer', pointer)
+        })
+    })
 
     return (
         <>

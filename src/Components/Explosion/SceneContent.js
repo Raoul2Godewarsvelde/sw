7224cf1cf1c320be3fad5ac3d2a7bbb23f391e5e
MokoMaracucho <desmoulin.federico@gmail.com>
2022-06-16 09:04:49 +0200

    NO: trying to use bufferGeometry...

--- src/Components/Explosion/SceneContent.js
+++ src/Components/Explosion/SceneContent.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useMemo, useCallback } from 'react'
+
+import * as THREE from 'three'
 
 const SceneContent = () => {
+     
+    const vertices = useMemo(() => new Float32Array([-1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0]), [])
+    
+    /* const update = useCallback(self => {
+        self.needsUpdate = true
+        self.parent.computeBoundingSphere()
+    }, []) */
 
     return (
-        <mesh scale={[10, 10, 10]} onClick={console.log('ok')}>
-            <icosahedronBufferGeometry attach='geometry' args={[2, 2]} />
-            <meshNormalMaterial attach='material' color='hotpink' wireframe />
-        </mesh>
+        <>
+            <mesh scale={[10, 10, 10]} onClick={console.log('ok')}>
+                <icosahedronBufferGeometry attach='geometry' args={[2, 2]} />
+                <meshNormalMaterial attach='material' wireframe />
+            </mesh>
+            <mesh position={[0, 0 , 0]}>
+                <bufferGeometry attach='geometry' onUpdate={self => self.computeVertexNormals()}>
+                    <bufferAttribute
+                        attachObject={["attributes", "position"]}
+                        array={vertices}
+                        itemSize={3}
+                        /* onUpdate={update} */
+                    />
+                </bufferGeometry>
+                <meshNormalMaterial />
+            </mesh>
+        </>
     )
 }
 

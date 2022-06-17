0b7830bfff037af4ada76e20ed8cc2cb47a32165
MokoMaracucho <desmoulin.federico@gmail.com>
2022-06-17 21:39:18 +0200

    OK: FIRST POINT :)

--- src/Components/Explosion/SceneContent.js
+++ src/Components/Explosion/SceneContent.js
@@ -4,6 +4,9 @@ import * as THREE from 'three'
 import { useFrame } from '@react-three/fiber'
 
 const SceneContent = () => {
+    
+    const count = 500
+    let num = 0
 
     const vertices_number = 12
     const radius = 10
@@ -11,16 +14,17 @@ const SceneContent = () => {
     const H_ANGLE = PI / 180 * 72           // 72 degree = 360 / 5
     const V_ANGLE = Math.atan(1.0 / 2)      // elevation = 26.565 degree
 
+    const line = useRef()
+    const particle = useRef()
+
     const setArray = (vertices_number) => {
         const vertices_array = []
 
-        for(let i = 0; i < vertices_number; i++) {
-            vertices_array.push({
-                x: 0,
-                y: 0,
-                z: 0
-            })
+        for(let i = 0; i < vertices_number * 3; i++) {
+            vertices_array.push(0)
         }
+
+        console.log('vertices_array', vertices_array)
 
         return vertices_array
     }
@@ -31,13 +35,18 @@ const SceneContent = () => {
     let hAngle2 = -PI / 2;                // start from -90 deg at 2nd row
 
     // the first top vertex at (0, 0, r)
-    vertices_array[0] = {x: 0, y: 0, z: radius}
+    vertices_array[0] = 0
+    vertices_array[1] = 0
+    vertices_array[2] = 0
+    /* vertices_array[0] = {x: 0, y: 0, z: radius} */
     
-    for(let i = 1; i < vertices_number / 2; i++) {
+    /* for(let i = 1; i < vertices_number / 2; i++) {
         vertices_array[i] = {
-            x: i - 1
+            x: 
+            y: 
+            z: 
         }
-    }
+    } */
     
     vertices_array[vertices_number - 1] = {x: 0, y: 0, z: -radius}
 
@@ -51,8 +60,43 @@ const SceneContent = () => {
 
 
 
+    
+    const [position] = useMemo(() => {
+        const position = new Float32Array(1 * 3)
+        /* const sizes = new Float32Array(vertices_number * 3) */
 
-    const count = 500
+        position[0] = 0
+        position[1] = radius
+        position[2] = 0
+
+        /* for (let i = 0; i < count * 3; i++) {
+            position[i] = (Math.random() - 0.5) * 10
+            sizes[i] = Math.random() < 0.03 ? 15 : 6
+        } */
+
+        return [position/* , sizes */]
+    }, [])
+
+    /* useFrame(() => {
+        if (line.current && num < count && particle.current) {
+            line.current.setDrawRange(0, 10)
+            particle.current.setDrawRange(0, 50)
+            num++
+        }
+    }) */
+
+
+
+
+
+
+
+
+
+
+
+
+    /* const count = 500
     let num = 0
 
     const line = useRef()
@@ -76,7 +120,7 @@ const SceneContent = () => {
             particle.current.setDrawRange(0, 50)
             num++
         }
-    })
+    }) */
 
     return (
         <>
@@ -85,29 +129,29 @@ const SceneContent = () => {
                 <meshNormalMaterial attach='material' wireframe />
             </mesh>
             <group>
-                <lineSegments>
+                {/* <lineSegments>
                     <bufferGeometry ref={line}>
                     <bufferAttribute
                         attach='attributes-position'
-                        count={positions.length / 3}
-                        array={positions}
+                        count={position.length / 3}
+                        array={position}
                         itemSize={3}
                     />
                     </bufferGeometry>
                     <lineBasicMaterial
-                    attach='material'
-                    color={'#9c88ff'}
-                    linewidth={0.3}
+                        attach='material'
+                        color={'#9c88ff'}
+                        linewidth={0.3}
                     />
-                </lineSegments>
+                </lineSegments> */}
                 <points>
                     <bufferGeometry ref={particle}>
-                    <bufferAttribute
-                        attach='attributes-position'
-                        count={positions.length / 3}
-                        array={positions}
-                        itemSize={3}
-                    />
+                        <bufferAttribute
+                            attach='attributes-position'
+                            count={position.length / 3}
+                            array={position}
+                            itemSize={3}
+                        />
                     </bufferGeometry>
                     <pointsMaterial size={0.1} />
                 </points>
